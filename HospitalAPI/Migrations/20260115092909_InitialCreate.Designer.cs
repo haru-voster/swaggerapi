4f2102802901287d43df58fbd26d70be01a0f571
haru-voster <wabwireharoun2001@gmail.com>
2026-01-16 21:56:49 +0300

    tables $ migrations

--- HospitalAPI/Migrations/20260115092909_InitialCreate.Designer.cs
+++ HospitalAPI/Migrations/20260115092909_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HospitalAPI.Migrations
 {
     [DbContext(typeof(DataApplicationDBContext))]
-    [Migration("20260115071549_InitialCreate")]
+    [Migration("20260115092909_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
